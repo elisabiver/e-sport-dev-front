bca4e0247eed32a5c1ba7c9c5859acc5148dc73b
Elisa Biver <elisa.biver@heig-vd.com>
2020-01-12 19:15:11 +0100

    légères modifs create-tournament

--- src/app/home/create-tournament/create-tournament.page.ts
+++ src/app/home/create-tournament/create-tournament.page.ts
@@ -4,6 +4,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { TournamentService } from 'src/app/tournament.service'
 import { HttpClient } from '@angular/common/http';
 import { Tournament } from 'src/app/models/tournament';
+import { Geolocation } from '@ionic-native/geolocation/ngx'
 
 
 @Component({
@@ -15,16 +16,34 @@ export class CreateTournamentPage implements OnInit {
 
   tournaments: Tournament[];
   
-  constructor(private tournamentService: TournamentService, public http: HttpClient) {
+  constructor(private tournamentService: TournamentService, 
+    private http: HttpClient,
+    private geolocation: Geolocation
+    ) {
     this.tournaments = [];
    }
 
   ngOnInit() {
+
   }
 
-  createTournament(form: NgForm){
+/*   createTournament(form: NgForm){
     console.log(form);
     this.tournamentService.createTournament(form.value).subscribe();
     console.log("test")
+  } */
+
+  createTournament(form: NgForm){
+    let payload = {
+      "name": form.value.name,
+      "location": {
+        "type": form.value.adress, //ca marche pas --> erreur 500 /* form.controls['adress'].value, */
+        "coordinates": form.value.coordinates /* form.controls['coordinates'].value */
+      },
+      "teams": form.value.teams,
+    };
+
+    this.tournamentService.createTournament(payload).subscribe();
+    console.log("test");
   }
 }
