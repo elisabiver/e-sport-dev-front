398be508071d1149d84ccba31c1a6ecc63348b48
Adrien Lestuzzi <adrien.lestuzzi@heig-vd.ch>
2020-01-13 16:26:18 +0100

    ajout de coordonnées fonctionnel

--- src/app/home/create-tournament/create-tournament.page.ts
+++ src/app/home/create-tournament/create-tournament.page.ts
@@ -4,7 +4,10 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { TournamentService } from 'src/app/tournament.service'
 import { HttpClient } from '@angular/common/http';
 import { Tournament } from 'src/app/models/tournament';
-import { Geolocation } from '@ionic-native/geolocation/ngx'
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Geoposition } from '@ionic-native/geolocation/ngx';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { empty } from 'rxjs';
 
 
 @Component({
@@ -15,29 +18,58 @@ import { Geolocation } from '@ionic-native/geolocation/ngx'
 export class CreateTournamentPage implements OnInit {
 
   tournaments: Tournament[];
+  c: Coordinates;
   
-  constructor(private tournamentService: TournamentService, 
-    private http: HttpClient,
-    private geolocation: Geolocation
-    ) {
+  constructor(private tournamentService: TournamentService, private http: HttpClient, private geolocation: Geolocation) {
     this.tournaments = [];
    }
 
   ngOnInit() {
-
+    this.geolocation.getCurrentPosition().then((position: Geoposition) => {
+      this.c = position.coords;
+      // console.log(this.c.latitude, this.c.longitude);
+    }).catch(err => {
+      console.warn(`Could not retrieve user position because: ${err.message}`);
+    }); 
   }
 
   createTournament(form: NgForm){
-    let payload = {
-      "name": form.value.name,
-      "location": {
-        "type": form.value.adress, //ca marche pas --> erreur 500 /* form.controls['adress'].value, */
-        "coordinates": form.value.coordinates /* form.controls['coordinates'].value */
-      },
-      "teams": form.value.teams,
-    };
+    var long = 0;
+    var lat = 0;
+    if(form.value.latitude == empty){
+        long = this.c.latitude 
+    }else{
+        long = form.value.latitude
+    }
 
-    this.tournamentService.createTournament(payload).subscribe();
-    console.log("test");
+    if(form.value.longitude == empty){
+      long = this.c.longitude 
+    }else{
+      long = form.value.longitude
+    }
+    
+    let payload = {
+      location : {
+        type : "Point",
+        coordinates : [lat , long]
+      },
+      teams: form.value.teams,
+      name : form.value.name,
+      // {
+      //   "location": {
+      //     "type": "Point",
+      //     "coordinates": [
+      //         -73.856077,
+      //         40.848447
+      //     ]
+      // },
+     
+  };
+
+    
+  this.tournamentService.createTournament(payload).subscribe();
+    
   }
+  
+
 }
