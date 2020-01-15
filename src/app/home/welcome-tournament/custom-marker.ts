137d148db20f07e051617ee3625871116f9e521e
Adrien Lestuzzi <adrien.lestuzzi@heig-vd.ch>
2020-01-15 16:57:06 +0100

    icone custom pour l'utlilisateur

--- src/app/home/welcome-tournament/custom-marker.ts
+++ src/app/home/welcome-tournament/custom-marker.ts
@@ -1,13 +1,15 @@
 import { Icon, IconOptions, icon } from 'leaflet';
-export const defaultIcon: Icon<IconOptions> = icon({
+export const customIcon: Icon<IconOptions> = icon({
   // This define the displayed icon size, in pixel
-  iconSize: [ 25, 41 ],
+  iconSize: [ 25, 25 ],
   // This defines the pixel that should be placed right above the location
   // If not provided, the image center will be used, and that could be awkward
-  iconAnchor: [ 13, 41 ],
-  popupAnchor:  [0, -25],
+  iconAnchor: [ 13, 25 ],
+  shadowSize:   [25, 25],
+  shadowAnchor: [5, 25],
+  popupAnchor:  [0, -20],
   // The path to the image to display. In this case, it's a Leaflet asset
-  iconUrl: 'leaflet/marker-icon.png',
+  iconUrl: 'assets/markers/user.png',
   // The path to the image's shadow to display. Also a leaflet asset
-  shadowUrl: 'leaflet/marker-shadow.png'
+  shadowUrl: 'assets/markers/user_shadow.png'
 });
